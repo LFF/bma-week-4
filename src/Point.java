c7d33bcc19f1226a701d93dd3d19420a61d7b14e
luisfonseca <luisfonseca@192.168.1.208>
2012-10-04 12:57:16 -0400

    example of default constructor

---
+++ src/Point.java
@@ -0,0 +1,29 @@
+
+public class Point {
+
+	/**
+	 * @param args
+	 * 
+	 * example of default constructor 
+	 * 
+	 */
+	public static void main(String[] args) {
+		
+		Point point = new Point();
+		
+		point.x = 10;
+		point.y = 33;
+		
+		
+					
+		
+		// TODO Auto-generated method stub
+
+	}
+	
+	int x;
+	int y;
+	
+	
+
+}
